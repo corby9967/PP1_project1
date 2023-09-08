ee5b08e4347c5802701099e0c86382e48ec61088
corby9967 <corby9967@gmail.com>
2023-09-08 16:20:11 +0900

    C2

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -12,21 +12,20 @@ public class WordCRUD implements ICRUD{
 
     @Override
     public Object add() {
-//        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
-//        int level = s.nextInt();
-//        String word = s.nextLine();
-//
-//        System.out.print("뜻 입력 : ");
-//        String meaning = s.nextLine();
-//
-//        return new Word(0, level, word, meaning);
-        return null;
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        return new Word(0, level, word, meaning);
     }
 
     public void addWord(){
-//        Word one = (Word)add();
-//        list.add(one);
-//        System.out.println("새 단어가 단어장에 추가되었습니다. ");
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다. ");
     }
 
     public void listAll(){
